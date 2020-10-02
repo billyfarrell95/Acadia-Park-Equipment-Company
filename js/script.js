bdc1851d634a6e521a1bbfbc6515e423cfd60f61
billyfarrell95 <billyfarrell95@gmail.com>
2020-10-01 19:01:20 -0500

    Update script.js
    added code comments

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
+// constants for the different form components. These are used to check the input.
 const name = document.getElementById('name')
 const email = document.getElementById('email')
 const subject = document.getElementById('subject')
 const message = document.getElementById('message')
 const form = document.getElementById('form')
 
+// defines elements based on IDs. Error message are shown when these constants are called and parameters are or aren't met 
 const nameErrorElement = document.getElementById('name-error')
 const messageErrorElement = document.getElementById('message-error')
 const emailErrorElement = document.getElementById('email-error')
 
-/*prevent page from submitting if there are errors 
-form validation without HTML "required" property */
-
-// name validation
-form.addEventListener('submit', (e) => {
-    let nameErrorMessage = []
-    if (name.value === '' || name.value == null) {
+// name validation (used to display message if there isn't input)
+form.addEventListener('submit', (e) => {  // doesn't allow incomplete form to be submitted
+    let nameErrorMessage = []  // empty array that can be filled after validation
+    if (name.value === '' || name.value == null) { // ff the input field is empty, the message will be displayed
         nameErrorMessage.push('Name is required')
     }
     
-    if (nameErrorMessage.length > 0) {
-        e.preventDefault()  
-        nameErrorElement.innerText = nameErrorMessage
+    if (nameErrorMessage.length > 0) {  // checks if there is an error message present
+        e.preventDefault()  // prevents incomplete form from being submitted
+        nameErrorElement.innerText = nameErrorMessage  // displays error message
     }
 })
 
-// email validation (required only)
+// email validation (used to display message if there isn't input)
 
 form.addEventListener('submit', (e) => {
-    let emailErrorMessage = []
+    let emailErrorMessage = [] // creates an array to store
     if (email.value === '' || email.value == null) {
         emailErrorMessage.push('Email is required')
     }
@@ -38,7 +37,7 @@ form.addEventListener('submit', (e) => {
     }
 })
 
-// message validation
+// message validation (used to display message if there isn't input)
 form.addEventListener('submit', (e) => {
     let messageErrorMessage = []
     if (message.value === '' || message.value == null) {
